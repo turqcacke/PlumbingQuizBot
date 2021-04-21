36e3c1e0f3109b0bec51913ed381f24b2a02748d
turqcacke <60967566+turqcacke@users.noreply.github.com>
2021-04-21 20:14:34 +0500

    fix: q2 ans kb

--- keyboards/default/q2_answers.py
+++ keyboards/default/q2_answers.py
@@ -6,6 +6,6 @@ def generate_q2_ans(row_width=1, lang='ru'):
     kb = ReplyKeyboardMarkup(row_width=row_width, resize_keyboard=True)
     kb.insert(KeyboardButton(_('q2a1', locale=lang)))
     kb.insert(KeyboardButton(_('q2a2', locale=lang)))
-    kb.insert(KeyboardButton(_('q2a3'), locale=lang))
+    kb.insert(KeyboardButton(_('q2a3', locale=lang)))
     kb.insert(KeyboardButton(_('back', locale=lang)))
     return kb
