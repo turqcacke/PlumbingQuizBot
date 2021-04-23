820e7f0180d21720c39c4d7ac6eb296df8674cee
turqcacke <60967566+turqcacke@users.noreply.github.com>
2021-04-23 17:49:48 +0500

    upd: add logger level in back_handler

--- handlers/user/back.py
+++ handlers/user/back.py
@@ -5,6 +5,7 @@ from middlewares import _
 from keyboards.default import generate_lang_keyboard
 from utils.misc.polls import stop_poll
 from utils.misc.quiz_utils import go_to_last_state
+from loguru import logger
 
 
 async def back_handler(message: types.Message, state: FSMContext):
@@ -13,9 +14,17 @@ async def back_handler(message: types.Message, state: FSMContext):
     if cur_state == GeneralStates.confirm_answer.state:
         if data[UserDataConsts.LAST_QUESTION] == QuizStates.q3.state:
             await stop_poll(message.chat.id, data)
-        await go_to_last_state(message, state, data)
+        try:
+            await go_to_last_state(message, state, data)
+        except Exception as e:
+            logger.debug(data)
+            raise e
     elif cur_state != QuizStates.q1.state:
-        await go_to_last_state(message, state, data)
+        try:
+            await go_to_last_state(message, state, data)
+        except Exception as e:
+            logger.debug(data)
+            raise e
     else:
         await GeneralStates.language.set()
         await message.answer(_('greetings', locale='ru') + '\n\n' + _('greetings', locale='uz'),
